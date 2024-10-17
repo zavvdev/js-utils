eec29f0691c1acf19395efec5d1d5a9856f4bfca
zavvdev <zaviryukha.igor99@gmail.com>
2024-10-17 23:39:10 +0300

    add not, match

--- fp.js
+++ fp.js
@@ -1,57 +1,79 @@
 /**
- * @template {Base} T
+ * @template {unknown} T
  * @param {T} value
  * @param  {...(value: T) => T} fns
  * @returns {T}
  */
-export const pipe = (value, ...fns) => fns.reduce((r, f) => f(r), value);
+export var pipe = (value, ...fns) => fns.reduce((r, f) => f(r), value);
 
 /**
- * @template {Base} T
+ * @template {unknown} T
  * @param {T} values
  * @returns {T}
  */
-export const identity = (value) => value;
+export var identity = (value) => value;
 
 /**
- * @template {Base} T
- * @param {(T) => T} elseClause
- * @param {Array<[(T) => boolean, (T) => T]>} ifClauses
- * @retuns (T) => T
+ * @template {unknown} T
+ * @param {T} value
+ * @returns {boolean}
  */
-export const cond =
+export var not = (value) => !value;
+
+/**
+ * @template {unknown} T
+ * @param {(T) => T} elseClause
+ * @param {Array<[(T) => boolean, (value: T) => T]>} ifClauses
+ * @retuns {(value: T) => T}
+ */
+export var cond =
   (elseClause, ...ifClauses) =>
-    (value) => {
-      return (
-        ifClauses.find((ifClause) => ifClause[0](value))?.[1]?.(value) ||
-        elseClause(value)
-      );
-    };
+  (value) => {
+    return (
+      ifClauses.find((ifClause) => ifClause[0](value))?.[1]?.(value) ||
+      elseClause(value)
+    );
+  };
+
+/**
+ * @template {string|number|null|undefined|boolean} T
+ * @template {unknown} R
+ * @template {unknown} F
+ * @param {F} fallback
+ * @param {Array<[T, (value: T) => R]>} patterns
+ * @retuns (value: T) => R
+ */
+export var match =
+  (fallback, ...patterns) =>
+  (value) => {
+    const matched = patterns.find((pattern) => pattern[0] === value);
+    return matched ? matched[1](value) : fallback;
+  };
 
 /**
  * Maybe monad
  */
-export const Maybe = (() => {
-  const M = function(value) {
+export var Maybe = (() => {
+  const M = function (value) {
     /**
      * @private
      */
     this.__v = value;
   };
 
-  M.of = function(value) {
+  M.of = function (value) {
     return new M(value);
   };
 
-  M.prototype.isNone = function() {
+  M.prototype.isNone = function () {
     return this.__v === null || this.__v === undefined;
   };
 
-  M.prototype.value = function() {
+  M.prototype.value = function () {
     return this.__v;
   };
 
-  M.prototype.map = function(f) {
+  M.prototype.map = function (f) {
     if (this.isNone()) {
       return M.of(this.__v);
     }
@@ -59,7 +81,7 @@ export const Maybe = (() => {
     return M.of(f(this.__v));
   };
 
-  M.prototype.orElse = function(fallback) {
+  M.prototype.orElse = function (fallback) {
     if (this.isNone()) {
       return M.of(fallback);
     }
@@ -69,4 +91,3 @@ export const Maybe = (() => {
 
   return M;
 })();
-
