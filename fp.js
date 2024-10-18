02fd1dcfa147a014c0001b9d483d5f4419b48ae7
zavvdev <zaviryukha.igor99@gmail.com>
2024-10-18 22:11:10 +0300

    fp

--- fp.js
+++ fp.js
@@ -1,80 +1,4 @@
 /**
- * @template {unknown} T
- * @template {unknown} R
- * @param {Array<Function>} fns
- * @returns {(x: T) => T | R}
- */
-export var pipe =
-  (...fns) =>
-  (x) =>
-    fns.reduce((r, f) => f(r), x);
-
-/**
- * @template {unknown} T
- * @template {unknown} R
- * @param {Array<Function>} fns
- * @returns {(x: T) => T | R}
- */
-export var compose =
-  (...fns) =>
-  (x) =>
-    fns.reduceRight((r, f) => f(r), x);
-
-/**
- * @template {unknown} T
- * @param {T} x
- * @returns {T}
- */
-export var identity = (x) => x;
-
-/**
- * @template {unknown} T
- * @param {T} x
- * @returns {boolean}
- */
-export var not = (x) => !x;
-
-/**
- * @template {unknown} T
- * @param {(T) => T} elseClause
- * @param {Array<[(T) => boolean, (x: T) => T]>} ifClauses
- * @retuns {(x: T) => T}
- */
-export var cond =
-  (elseClause, ...ifClauses) =>
-  (x) =>
-    ifClauses.find((ifClause) => ifClause[0](x))?.[1]?.(x) || elseClause(x);
-
-/**
- * @template {string|number|null|undefined|boolean} T
- * @template {unknown} R
- * @template {unknown} F
- * @param {F} fallback
- * @param {Array<[T, (x: T) => R]>} patterns
- * @retuns {(x: T) => R}
- */
-export var match =
-  (fallback, ...patterns) =>
-  (x) =>
-    patterns.find((pattern) => pattern[0] === x)?.[1]?.(x) || fallback;
-
-/**
- * @param {Function} fn
- * @returns {Function}
- */
-export var curry = (fn) => {
-  var arity = fn.length;
-
-  return function result(...args) {
-    if (args.length < arity) {
-      return result.bind(null, ...args);
-    }
-
-    return fn.call(null, ...args);
-  };
-};
-
-/**
  * Maybe monad
  */
 export var Maybe = (() => {
@@ -115,3 +39,103 @@ export var Maybe = (() => {
 
   return M;
 })();
+
+/**
+ * @template {unknown} T
+ * @template {unknown} R
+ * @param {Array<Function>} fns
+ * @returns {(x: T) => T | R}
+ */
+export var pipe =
+  (...fns) =>
+  (x) =>
+    fns.reduce((r, f) => f(r), x);
+
+/**
+ * @template {unknown} T
+ * @template {unknown} R
+ * @param {Array<Function>} fns
+ * @returns {(x: T) => T | R}
+ */
+export var compose =
+  (...fns) =>
+  (x) =>
+    fns.reduceRight((r, f) => f(r), x);
+
+/**
+ * @template {unknown} T
+ * @template {unknown} R
+ * @param {(v: T) => R} fn
+ * @returns {(xs: Array<T>) => Array<R>}
+ */
+export var map = (fn) => (xs) => xs.map(fn);
+
+/**
+ * @template {unknown} T
+ * @param {T} x
+ * @returns {T}
+ */
+export var identity = (x) => x;
+
+/**
+ * @template {unknown} T
+ * @param {T} x
+ * @returns {boolean}
+ */
+export var not = (x) => !x;
+
+/**
+ * @template {Object} T
+ * @template {unknown} R
+ * @param {string} k
+ * @returns {(x: T) => R}
+ */
+export var prop = (k) => (x) => x[k];
+
+/**
+ * @template {Object} T
+ * @template {unknown} R
+ * @param {string} k
+ * @returns {(x: T) => Maybe(R)}
+ */
+export var safeProp = (k) => (x) => Maybe.of(x[k]);
+
+/**
+ * @template {unknown} T
+ * @param {(T) => T} elseClause
+ * @param {Array<[(T) => boolean, (x: T) => T]>} ifClauses
+ * @retuns {(x: T) => T}
+ */
+export var cond =
+  (elseClause, ...ifClauses) =>
+  (x) =>
+    ifClauses.find((ifClause) => ifClause[0](x))?.[1]?.(x) || elseClause(x);
+
+/**
+ * @template {string|number|null|undefined|boolean} T
+ * @template {unknown} R
+ * @template {unknown} F
+ * @param {F} fallback
+ * @param {Array<[T, (x: T) => R]>} patterns
+ * @retuns {(x: T) => R}
+ */
+export var match =
+  (fallback, ...patterns) =>
+  (x) =>
+    patterns.find((pattern) => pattern[0] === x)?.[1]?.(x) || fallback;
+
+/**
+ * @param {Function} fn
+ * @returns {Function}
+ */
+export var curry = (fn) => {
+  var arity = fn.length;
+
+  return function result(...args) {
+    if (args.length < arity) {
+      return result.bind(null, ...args);
+    }
+
+    return fn.call(null, ...args);
+  };
+};
