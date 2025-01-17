9b3c9f8bafea141acf26cf6a0186f7d3fc66f191
zavvdev <zaviryukha.igor99@gmail.com>
2025-01-17 21:50:32 +0200

    dom

---
+++ dom.js
@@ -0,0 +1,50 @@
+export const domUtil = {
+  /**
+   * @param {string} name
+   * @returns {string | undefined}
+   */
+  getMetaTagContent: (name) => {
+    if (!global?.window) {
+      return undefined;
+    }
+
+    const meta = Array.from(document.querySelectorAll("meta[name]")).find(
+      (metaItem) => metaItem.getAttribute("name") === name,
+    );
+
+    return meta?.getAttribute("content") || undefined;
+  },
+
+  /**
+   * @param {string} name
+   * @returns {string | undefined}
+   */
+  getLinkTagHref: (name) => {
+    if (!global?.window) {
+      return undefined;
+    }
+
+    const link = Array.from(document.querySelectorAll("link[rel]")).find(
+      (link) => link?.getAttribute("rel") === name,
+    );
+
+    return link?.getAttribute("href") || undefined;
+  },
+
+  /**
+   * @param {string} url
+   * @param {string} fileName
+   * @returns {void}
+   */
+  downloadFromUrl: (url, fileName) => {
+    if (typeof url === "string" && typeof fileName === "string") {
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = fileName;
+      document.body.append(link);
+      link.click();
+      link.remove();
+      URL.revokeObjectURL(url);
+    }
+  },
+};
