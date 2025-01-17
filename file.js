c82cbd83aa245b599729d38ed877657836ee70f9
zavvdev <zaviryukha.igor99@gmail.com>
2025-01-17 21:53:10 +0200

    file

--- file.js
+++ file.js
@@ -41,6 +41,59 @@ class FileUtil {
       reader[methodsMap[method]](file);
     });
   };
+
+  /**
+   * @param {string} base64
+   * @param {"pdf"} convertType
+   * @returns {Blob}
+   */
+  base64ToBlob = (base64, convertType) => {
+    if (typeof base64 === "string") {
+      const byteCharacters = atob(base64);
+      const byteNumbers = Array.from(
+        byteCharacters,
+        (char) => char.codePointAt(0) || 0,
+      );
+
+      const byteArray = new Uint8Array(byteNumbers);
+      return new Blob([byteArray], { type: `application/${convertType}` });
+    }
+
+    throw new TypeError(`not string type`);
+  };
+
+  /**
+   * @param {Blob} blob
+   * @returns {string}
+   */
+  blobToUrl = (blob) => {
+    if (blob instanceof Blob) {
+      return URL.createObjectURL(blob);
+    }
+    throw new TypeError(`Not blob type`);
+  };
+
+  /**
+   * @param {string} dataUrl
+   * @param {string} fileName
+   * @returns {File}
+   */
+  dataUrlToFile = (dataUrl, fileName) => {
+    const array = dataUrl.split(",");
+    const regex = /:(.*?);/;
+    const match = array?.[0] ? regex.exec(array?.[0]) : null;
+    const mime = match?.[1];
+    const bstr = atob(array[1]);
+
+    let n = bstr.length;
+    const u8array = new Uint8Array(n);
+
+    while (n--) {
+      u8array[n] = bstr.codePointAt(n);
+    }
+
+    return new File([u8array], fileName, { type: mime });
+  };
 }
 
 export const fileUtil = new FileUtil();
